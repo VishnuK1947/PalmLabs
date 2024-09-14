67d6d1a01c449c9548b0ea65805ce741a029122a
Grace Li <gracemxli51@gmail.com>
2024-09-14 16:02:51 -0400

    landing page

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -9,22 +9,22 @@ export function Layout({
   children: ReactNode;
 }) {
   return (
-    <div className="flex h-screen w-full flex-col">
-      <header className="sticky top-0 z-10 flex min-h-20 border-b bg-background/80 backdrop-blur">
-        <nav className="container w-full justify-between flex flex-row items-center gap-6">
+    <div className="flex h-screen w-full flex-col bg-gradient-to-b from-cyan-300 via-white-300 to-yellow-300 text-white">
+      <header className="sticky top-0 z-10 flex min-h-20 backdrop-blur">
+        <nav className="container w-full justify-between flex flex-row items-center gap-0">
           <div className="flex items-center gap-6 md:gap-10">
             <a href="/">
               <h1 className="text-base font-semibold">React Template</h1>
             </a>
             <div className="flex items-center gap-4 text-sm">
               <GetStartedDialog>
-                <button className="text-muted-foreground transition-colors hover:text-foreground">
+                <button className="transition-colors hover:text-gray-300">
                   Help
                 </button>
               </GetStartedDialog>
               <a
                 href="https://docs.convex.dev"
-                className="text-muted-foreground transition-colors hover:text-foreground"
+                className="transition-colors hover:text-gray-300"
                 target="_blank"
               >
                 Docs
@@ -34,15 +34,18 @@ export function Layout({
           {menu}
         </nav>
       </header>
-      <main className="flex grow flex-col overflow-hidden">{children}</main>
-      <footer className="border-t hidden sm:block">
+      <main className="flex grow flex-col overflow-hidden">
+        {/* Add big title at the top */}
+        <div className="flex justify-center items-center p-4 mt-20">
+            <img src="public/PALMS22.png" alt="PalmLabs Logo" className="h-32 mr-4" />
+            <h1 className="text-4xl font-bold [font-family:'PortLligat_Slab-Regular',Helvetica] text-white text-[150px]">PalmLabs</h1>
+        </div>
+        {children}
+      </main>
+      <footer className="hidden sm:block">
         <div className="container py-4 text-sm leading-loose">
-          Built with ❤️ at{" "}
+          Built with {" "}
           <FooterLink href="https://www.convex.dev/">Convex</FooterLink>.
-          Powered by Convex,{" "}
-          <FooterLink href="https://vitejs.dev">Vite</FooterLink>,{" "}
-          <FooterLink href="https://react.dev/">React</FooterLink> and{" "}
-          <FooterLink href="https://ui.shadcn.com/">shadcn/ui</FooterLink>.
         </div>
       </footer>
     </div>
